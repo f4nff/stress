afb20dab311879656b245d4b2db3566e5ddf5720
f4nff <mobobye@gmail.com>
2017-08-20 09:31:14 -0500

    Add files via upload

--- stress.go
+++ stress.go
@@ -1,255 +1,279 @@
-package main
+package stress
 
 import (
-	"flag"
-	"fmt"
+	"bytes"
+	"crypto/tls"
+	"io"
 	"io/ioutil"
 	"net/http"
-	gurl "net/url"
+	"net/http/httptrace"
+	"net/url"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
+	"sync"
 	"time"
 
-	lbstress "github.com/wenjiax/stress/stress"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/proxy"
 )
 
-var (
-	m = flag.String("m", "GET", "")
-	// headers  = flag.String("h", "", "")
-	body     = flag.String("b", "", "")
-	bodyFile = flag.String("B", "", "")
+type (
+	//Task contains the stress test configuration and request configuration.
+	Task struct {
+		// Request is the request to be made.
+		Request *http.Request
+		//ReqBody is the request of body.
+		ReqBody []byte
+		//Nuber is the total number of requests to send.
+		Number int
+		//Concurrent is the concurrent number of requests.
+		Concurrent int
+		Output     string
+		//Timeout is the timeout of request in seconds.
+		Timeout int
+		//ThinkTime is the think time of request in seconds.
+		ThinkTime int
+		// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
+		ProxyAddr *url.URL
+		Dialers   []proxy.Dialer
+		//H2 is an option to make HTTP/2 requests.
+		H2 bool
+		//DisableCompression is an option to disable compression in response.
+		DisableCompression bool
+		//DisableKeepAlives is an option to prevents re-use of TCP connections between different HTTP requests.
+		DisableKeepAlives bool
+		//DisableRedirects is an option to prevent the following of HTTP redirects.
+		DisableRedirects bool
 
-	output    = flag.String("o", "", "")
-	proxyAddr = flag.String("x", "", "")
-	host      = flag.String("host", "", "")
-
-	n         = flag.Int("n", 100, "")
-	c         = flag.Int("c", 10, "")
-	t         = flag.Int("t", 20, "")
-	d         = flag.Int("d", 0, "")
-	thinkTime = flag.Int("think-time", 0, "")
-
-	h2                 = flag.Bool("h2", false, "")
-	disableCompression = flag.Bool("disable-compression", false, "")
-	disableKeepalive   = flag.Bool("disable-keepalive", false, "")
-	disableRedirects   = flag.Bool("disable-redirects", false, "")
-	enableTran         = flag.Bool("enable-tran", false, "")
+		//The total think time required for all requests.
+		thinkDuration time.Duration
+		start         time.Time
+		results       chan *Result
+	}
 )
 
-const (
-	headerRegexp = `^([\w-]+):\s*(.+)`
-
-	methodsRegexp   = `m:([a-zA-Z]+),*`
-	bodyRegexp      = `b:([^,]+),*`
-	bodyFileRegexp  = `B:([^,]+),*`
-	proxyAddrRegexp = `x:([^,]+),*`
-	thinkTimeRegexp = `thinkTime:([\d]+),*`
-)
-
-var usage = `Usage: stress [options...] <url> || stress [options...] -enable-tran <urls...>
-
-Options:
-  -n  Number of requests to run. Default value is 100.
-      If set to -1, the request has been sent, but the report will 
-      not be output by default.
-  -c  Number of requests to run concurrently. 
-      Total number of requests cannot smaller than the concurrency level. 
-      Default value is 10.
-  -d  Duration of requests to run. Default value is 0 sec.
-  -o  Output file path. For example: /home/user or ./files.
-  
-  -h  Custom HTTP header. For example: 
-      -h "Accept: text/html" -h "Content-Type: application/xml".
-  -m  HTTP method, any of GET, POST, PUT, DELETE, HEAD, OPTIONS.
-  -t  Timeout for each request in seconds. Default value is 20, 
-      use 0 for infinite.
-  -b  HTTP request body.
-  -B  HTTP request body from file. For example:
-      /home/user/file.txt or ./file.txt.
-  -x  HTTP Proxy address as host:port.
-
-  -h2 	 Enable HTTP/2.
-  -host	 Set HTTP Host header.
-  
-  -think-time           Time to think after request. Default value is 0 sec.
-  -disable-compression  Disable compression.
-  -disable-keepalive    Disable keep-alive, prevents re-use of TCP
-                    	connections between different HTTP requests.
-  -disable-redirects    Disable following of HTTP redirects.
-  -enable-tran          Enable transactional requests. Multiple urls 
-                        form a transactional requests. 
-                        For example: "stress [options...] -enable-tran 
-                        http://localhost:8080,m:post,b:hi,x:http://127.0.0.1:8888 
-                        http://localhost:8888,m:post,B:/home/file.txt,thinkTime:2 
-                        [urls...]".
-`
-
-func main() {
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, usage)
+//Run is run a task.
+func (t *Task) Run() error {
+	if t.Number > 0 {
+		t.results = make(chan *Result, t.Number)
 	}
-	var hs headerSlice
-	flag.Var(&hs, "h", "")
-	flag.Parse()
-	if flag.NArg() <= 0 {
-		usageAndExit("")
-	}
-	//Parsing global request header.
-	header := make(http.Header)
-	// hs := strings.Split(*headers, ";")
-	for _, h := range hs {
-		match, err := parseInputWithRegexp(h, headerRegexp)
-		if err != nil {
-			usageAndExit(err.Error())
-		}
-		header.Set(match[1], match[2])
-	}
-	//Parsing global request proxyAddr.
-	var proxyURL *gurl.URL
-	if *proxyAddr != "" {
-		var err error
-		proxyURL, err = gurl.Parse(*proxyAddr)
-		if err != nil {
-			usageAndExit(err.Error())
-		}
-	}
-	//Set parameters and global configuration.
-	task := &lbstress.Task{
-		Number:             *n,
-		Concurrent:         *c,
-		Duration:           time.Duration(*d) * time.Second,
-		Output:             *output,
-		Timeout:            *t,
-		ThinkTime:          *thinkTime,
-		ProxyAddr:          proxyURL,
-		DisableCompression: *disableCompression,
-		DisableKeepAlives:  *disableKeepalive,
-		DisableRedirects:   *disableRedirects,
-		Host:               *host,
-		H2:                 *h2,
-	}
-	if *enableTran {
-		runTran(task, header)
-	} else {
-		run(task, header)
-	}
-
-}
-
-func run(task *lbstress.Task, header http.Header) {
-	//Parsing request body.
-	var bodyAll []byte
-	if *body != "" {
-		bodyAll = []byte(*body)
-	}
-	if *bodyFile != "" {
-		content, err := ioutil.ReadFile(*bodyFile)
-		if err != nil {
-			errAndExit(err.Error())
-		}
-		bodyAll = content
-	}
-	//Run task.
-	err := task.Run(&lbstress.RequestConfig{
-		URLStr:  flag.Args()[0],
-		Method:  *m,
-		ReqBody: bodyAll,
-		Header:  header,
-	})
-	if err != nil {
-		errAndExit(err.Error())
-	}
-}
-
-func runTran(task *lbstress.Task, header http.Header) {
-	var configs []*lbstress.RequestConfig
-	for i, len := 0, flag.NArg(); i < len; i++ {
-		argstr := flag.Args()[i]
-		url := strings.Split(argstr, ",")[0]
-		//Parsing request method.
-		methodMatch, err := parseInputWithRegexp(argstr, methodsRegexp)
-		if err != nil {
-			errAndExit(err.Error())
-		}
-		//Parsing request body.
-		bodyMatch, _ := parseInputWithRegexp(argstr, bodyRegexp)
-		bodyFileMatch, _ := parseInputWithRegexp(argstr, bodyFileRegexp)
-		var bodyAll []byte
-		if bodyMatch != nil {
-			bodyAll = []byte(bodyMatch[1])
-		}
-		if bodyFileMatch != nil {
-			content, err := ioutil.ReadFile(bodyFileMatch[1])
-			if err != nil {
-				errAndExit(err.Error())
-			}
-			bodyAll = content
-		}
-		//Parsing request proxyAddr.
-		proxyAddrMatch, _ := parseInputWithRegexp(argstr, proxyAddrRegexp)
-		var proxyURL *gurl.URL
-		if proxyAddrMatch != nil {
-			var err error
-			proxyURL, err = gurl.Parse(*proxyAddr)
-			if err != nil {
-				usageAndExit(err.Error())
-			}
-		}
-		//Parsing request thinkTime.
-		thinkTime := 0
-		thinkTimeMatch, _ := parseInputWithRegexp(argstr, thinkTimeRegexp)
-		if thinkTimeMatch != nil {
-			thinkTime, _ = strconv.Atoi(thinkTimeMatch[1])
-		}
-		configs = append(configs, &lbstress.RequestConfig{
-			URLStr:    url,
-			Method:    methodMatch[1],
-			ReqBody:   bodyAll,
-			Header:    header,
-			ProxyAddr: proxyURL,
-			ThinkTime: thinkTime,
-		})
-	}
-	//Run transactional task.
-	err := task.RunTran(configs...)
-	if err != nil {
-		errAndExit(err.Error())
-	}
-}
-
-func parseInputWithRegexp(input, regx string) ([]string, error) {
-	re := regexp.MustCompile(regx)
-	matches := re.FindStringSubmatch(input)
-	if len(matches) < 1 {
-		return nil, fmt.Errorf("could not parse the provided input; input = %v", input)
-	}
-	return matches, nil
-}
-
-func usageAndExit(msg string) {
-	if msg != "" {
-		fmt.Fprintf(os.Stderr, msg)
-		fmt.Fprintf(os.Stderr, "\n\n")
-	}
-	flag.Usage()
-	fmt.Fprintf(os.Stderr, "\n")
-	os.Exit(1)
-}
-
-func errAndExit(msg string) {
-	fmt.Fprintf(os.Stderr, "Error:%s\n", msg)
-	os.Exit(1)
-}
-
-type headerSlice []string
-
-func (h *headerSlice) String() string {
-	return fmt.Sprintf("%s", *h)
-}
-
-func (h *headerSlice) Set(value string) error {
-	*h = append(*h, value)
+	t.start = time.Now()
+	t.runRequesters()
+	t.finish()
 	return nil
 }
+
+func (t *Task) finish() {
+	close(t.results)
+	if t.Number < 0 {
+		return
+	}
+	total := time.Now().Sub(t.start) - t.thinkDuration
+	newReport(os.Stdout, t.results, t.Output, total).finalize()
+}
+
+func (t *Task) runRequesters() {
+	dlen := len(t.Dialers)
+	if dlen > 0 {
+		t.Concurrent *= dlen
+	}
+	var wg sync.WaitGroup
+	wg.Add(t.Concurrent)
+	for i, j := 0, 0; i < t.Concurrent; i++ {
+		var dialer proxy.Dialer
+		if t.Dialers != nil {
+			dialer = t.Dialers[j]
+			j++
+			if j >= dlen {
+				j = 0
+			}
+		}
+		go func(dialer proxy.Dialer) {
+			t.runRequester(t.Number/t.Concurrent, dialer)
+			wg.Done()
+		}(dialer)
+	}
+	wg.Wait()
+}
+
+func (t *Task) runRequester(num int, dialer proxy.Dialer) {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		DisableCompression: t.DisableCompression,
+		DisableKeepAlives:  t.DisableKeepAlives,
+		Proxy:              http.ProxyURL(t.ProxyAddr),
+	}
+	if dialer != nil {
+		transport.Dial = dialer.Dial
+	}
+	if t.H2 {
+		http2.ConfigureTransport(transport)
+	} else {
+		transport.TLSNextProto = make(map[string]func(string, *tls.Conn) http.RoundTripper)
+	}
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   time.Duration(t.Timeout) * time.Second,
+	}
+	if t.DisableRedirects {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+	}
+	if t.Number < 0 {
+		for {
+			t.sendRequest(client)
+		}
+	} else {
+		for i := 0; i < num; i++ {
+			t.sendRequest(client)
+		}
+	}
+}
+
+func (t *Task) sendRequest(client *http.Client) {
+	var thinkDuration time.Duration
+	start := time.Now()
+	var size int64
+	var code int
+	var dnsStart, connStart, reqStart, resStart, delayStart time.Time
+	var dnsDuration, connDuration, reqDuration, resDuration, delayDuration time.Duration
+	req := cloneRequest(t.Request, t.ReqBody)
+	//Create httptrace.
+	trace := &httptrace.ClientTrace{
+		DNSStart: func(httptrace.DNSStartInfo) {
+			dnsStart = time.Now()
+		},
+		DNSDone: func(httptrace.DNSDoneInfo) {
+			dnsDuration = time.Now().Sub(dnsStart)
+		},
+		GetConn: func(h string) {
+			connStart = time.Now()
+		},
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			connDuration = time.Now().Sub(connStart)
+			reqStart = time.Now()
+		},
+		WroteRequest: func(w httptrace.WroteRequestInfo) {
+			reqDuration = time.Now().Sub(reqStart)
+			delayStart = time.Now()
+		},
+		GotFirstResponseByte: func() {
+			delayDuration = time.Now().Sub(delayStart)
+			resStart = time.Now()
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	res, err := client.Do(req)
+	if err == nil {
+		size = res.ContentLength
+		code = res.StatusCode
+		io.Copy(ioutil.Discard, res.Body)
+		res.Body.Close()
+	}
+	nowTime := time.Now()
+	resDuration = nowTime.Sub(resStart)
+	end := nowTime.Sub(start)
+	result := &Result{
+		URLStr:        req.URL.String(),
+		Method:        req.Method,
+		Err:           err,
+		StatusCode:    code,
+		Duration:      end,
+		ConnDuration:  connDuration,
+		DNSDuration:   dnsDuration,
+		ReqDuration:   reqDuration,
+		ResDuration:   resDuration,
+		DelayDuration: delayDuration,
+		ContentLength: size,
+	}
+	if t.Number > 0 {
+		t.results <- result
+	}
+	//Handle think time.
+	thinktime := time.Duration(t.ThinkTime) * time.Second
+	time.Sleep(thinktime)
+	thinkDuration += thinktime
+	t.thinkDuration += thinktime
+}
+
+func cloneRequest(r *http.Request, body []byte) *http.Request {
+	req := new(http.Request)
+	*req = *r
+	req.Header = make(http.Header, len(r.Header))
+	for k, s := range r.Header {
+		req.Header[k] = append([]string(nil), s...)
+	}
+	if len(body) > 0 {
+		req.Body = ioutil.NopCloser(bytes.NewReader(body))
+	}
+	return req
+}
+
+// func (t *Task) checkAndInitConfigs() error {
+// 	if t.Number == 0 && t.Duration <= 0 {
+// 		return errors.New("Number or Duration cannot be smaller than 1")
+// 	}
+// 	if t.Number != 0 && t.Duration > 0 {
+// 		return errors.New("Number and Duration only set one")
+// 	}
+// 	if t.Concurrent <= 0 {
+// 		return errors.New("Concurrent cannot be smaller than 1")
+// 	}
+// 	if t.Number > 0 && t.Number < t.Concurrent {
+// 		return errors.New("Number cannot be less than Concurrent")
+// 	}
+// 	if t.Number > 0 && t.Number%t.Concurrent != 0 {
+// 		return errors.New("Number must be an integer multiple of Concurrent")
+// 	}
+// 	if t.Output != "" {
+// 		err := os.MkdirAll(t.Output, 0777)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	if t.Duration <= 0 && t.Number > 0 {
+// 		t.results = make([]*Result, 0, t.Number)
+// 	}
+// 	for i, n := 0, len(t.reqConfigs); i < n; i++ {
+// 		if t.reqConfigs[i] == nil {
+// 			return errors.New("RequestConfig cannot be nil")
+// 		}
+// 		if t.reqConfigs[i].URLStr == "" || t.reqConfigs[i].Method == "" {
+// 			return errors.New("URLStr and Method cannot be empty")
+// 		}
+// 		if t.Timeout > 0 && t.reqConfigs[i].Timeout <= 0 {
+// 			t.reqConfigs[i].Timeout = t.Timeout
+// 		}
+// 		if t.ThinkTime > 0 && t.reqConfigs[i].ThinkTime <= 0 {
+// 			t.reqConfigs[i].ThinkTime = t.ThinkTime
+// 		}
+// 		if t.Host != "" && t.reqConfigs[i].Host == "" {
+// 			t.reqConfigs[i].Host = t.Host
+// 		}
+// 		if t.ProxyAddr != nil && t.reqConfigs[i].ProxyAddr == nil {
+// 			t.reqConfigs[i].ProxyAddr = t.ProxyAddr
+// 		}
+// 		if t.DisableCompression && !t.reqConfigs[i].DisableCompression {
+// 			t.reqConfigs[i].DisableCompression = true
+// 		}
+// 		if t.DisableKeepAlives && !t.reqConfigs[i].DisableKeepAlives {
+// 			t.reqConfigs[i].DisableKeepAlives = true
+// 		}
+// 		if t.DisableRedirects && !t.reqConfigs[i].DisableRedirects {
+// 			t.reqConfigs[i].DisableRedirects = true
+// 		}
+// 		t.reqConfigs[i].Method = strings.ToUpper(t.reqConfigs[i].Method)
+// 		req, err := http.NewRequest(t.reqConfigs[i].Method, t.reqConfigs[i].URLStr, nil)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if t.reqConfigs[i].Header != nil {
+// 			req.Header = t.reqConfigs[i].Header
+// 		}
+// 		t.reqConfigs[i].request = req
+// 	}
+
+// 	return nil
+// }
